2344e99da3f166a1f52c849a31782205abb51751
忘忧北萱草 <wybxc@qq.com>
2024-06-03 02:56:37 +0800

    feat: switch to rust-peg

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 mod utils;
 
-use utils::{parser, dump};
-
 use crate::utils::printer::SimpleAST;
 
 const ENGINE: &str = include_str!("../generator/gen.pl");
 
 const QUERY: &str = r#"
-main :- def_use(T, 3), portray_clause(T).
+main :- ###QUERY###, portray_clause(T).
 :- initialization(main).
 "#;
 
 #[cfg(test)]
 mod tests;
 
+/// example:
+///
+/// ```text
+/// cargo run -- "def_use(T, 3)"
+/// ```
 fn main() {
-    let code = format!("{ENGINE}\n{QUERY}");
+    let input = std::env::args().nth(1).expect("missing argument");
+    let query = QUERY.replace("###QUERY###", &input);
+    let code = format!("{ENGINE}\n{query}");
+
     let result = utils::engine::eval_code(&code);
-    let tree = parser::get_tree(&result);
+    dbg!(&result);
+
+    let tree = utils::parser::get_tree(&result);
     let stmts = SimpleAST::from_tree(tree);
     for stmt in stmts {
         println!("{}", stmt);
--- src/tests.rs
+++ src/tests.rs
@@ -2,8 +2,8 @@ use super::*;
 
 #[test]
 fn test_parser() {
-    let input = "t(n(0,block),t(n(1,def),nil,t(n(1,use),nil,nil)), nil)";
-    let tree = parser::get_tree(input);
+    let input = "t(n(0,block),t(n(1,def),nil,t(n(1,use),nil,nil)), nil).";
+    let tree = utils::parser::get_tree(input);
     let s = SimpleAST::from_tree(tree);
     assert_eq!(s.len(), 1);
     assert_eq!(s[0].to_string(), "{    decl x1;\n\n    use x1;\n\n}");
