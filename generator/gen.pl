b5eb1c5364271c37991bf93022135e875730a220
Sdywolf <chnfy@stu.pku.edu.cn>
2024-06-03 17:28:45 +0800

    Fix bug in generator/gen.pl. Parser should be modified.

--- generator/gen.pl
+++ generator/gen.pl
@@ -9,26 +9,26 @@
 in(X, Min, Max) :-
     X #>= Min, X #=< Max.
 
-node(n(K, use), K).
-node(n(K, def), K).
-node(n(0, block), 0).
-node(n(0, loop), 0).
-node(n(0, brk), 0).
+node(n(Id, K, use), K, Id).
+node(n(Id, K, def), K, Id).
+node(n(Id, 0, block), 0, Id).
+node(n(Id, 0, loop), 0, Id).
+node(n(Id, 0, brk), 0, Id).
 
-tree(nil, 0, Ks, Ks).
-tree(t(V, L, R), N, [K| Ks], NKs) :-
-    node(V, K),
+tree(nil, 0, Ks, Ks, Ids, Ids).
+tree(t(V, L, R), N, [K| Ks], NKs, [Id| Ids], NIds) :-
+    node(V, K, Id),
     N1 #>= 0,
     N2 #>= 0, 
     N #= N1 + N2 + 1,
-    tree(L, N1, Ks, TKs),
-    tree(R, N2, TKs, NKs).
+    tree(L, N1, Ks, TKs, Ids, TIds),
+    tree(R, N2, TKs, NKs, TIds, NIds).
 
 vis(T,T).
-vis(T1, t(_, T3, _)) :-
-    vis(T1, T3).
-vis(T1, t(_, _, T3)) :-
-    vis(T1, T3).
+vis(T, t(_, L, _)) :-
+    vis(T, L).
+vis(T, t(_, _, R)) :-
+    vis(T, R).
 
 inL(T1, t(_, T3, _)) :-
     vis(T1, T3).
@@ -49,29 +49,29 @@ fd_domain([N| Ns], Min, Max) :-
     fd_domain(Ns, Min, Max).
 
 def_use(T) :-
-    foreach((vis(S,T), S = t(n(K, use), _, _)), 
-        (vis(R,T), R = t(n(K, def), _, _), vis(S, R))).
+    foreach((vis(S,T), S = t(n(_, K, use), _, _)), 
+        (vis(R,T), R = t(n(_, K, def), _, _), vis(S, R))).
 
 brk_in_loop(T) :-
-    foreach((vis(S,T), S = t(n(_, brk), _, _)), 
-        (vis(R,T), R = t(n(_, loop), _, _), inL(S, R))).
+    foreach((vis(S,T), S = t(n(_, _, brk), _, _)), 
+        (vis(R,T), R = t(n(_, _, loop), _, _), inL(S, R))).
 
 well_form(nil).
-well_form(t(n(_, use), L, R)) :-
+well_form(t(n(_, _, use), L, R)) :-
     L = nil,
     well_form(R).
-well_form(t(n(_, def), L, R)) :-
+well_form(t(n(_, _, def), L, R)) :-
     L = nil, 
     well_form(R).
-well_form(t(n(_, brk), L, R)) :-
+well_form(t(n(_, 0, brk), L, R)) :-
     L = nil, 
     well_form(R).
-well_form(t(n(_, block), L, R)) :-
+well_form(t(n(_, 0, block), L, R)) :-
     well_form(L), well_form(R).
-well_form(t(n(_, loop), L, R)) :-
+well_form(t(n(_, 0, loop), L, R)) :-
     well_form(L), well_form(R).
 
-real_well_form(t(n(0, block), L, R)) :-
+real_well_form(t(n(_, 0, block), L, R)) :-
     well_form(L), well_form(R).
 
 is_subtree(T, T).
@@ -83,11 +83,19 @@ isnt_subtree(_, nil) :- true.
 isnt_subtree(T, t(_, L, R)) :-
     isnt_subtree(T, L), isnt_subtree(T, R).
 
+increasing([]).
+increasing([H1|[H2]]) :-
+    H1 #< H2.
+increasing([H1|[H2|T]]) :-
+    H1 #< H2, increasing([H2|T]).
+
 generator(T, N) :-
-    varlist(N, Keys),
+    varlist(N, Keys), varlist(N, Ids), 
+    increasing(Ids), 
     Max #= N - 1, 
-    fd_domain(Keys, 0, Max), 
-    tree(T, N, Keys, []),
+    fd_domain(Keys, 0, Max), fd_domain(Ids, 0, Max), 
+    label(Ids), 
+    tree(T, N, Keys, [], Ids, []),
     real_well_form(T),
     brk_in_loop(T),
     label(Keys), 
