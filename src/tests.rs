5054bf1906cefd8e372056ba5f159ca19dbbd4bb
Sdywolf <chnfy@stu.pku.edu.cn>
2024-06-04 09:50:37 +0800

    modifiy tests in src/tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ use super::*;
 
 #[test]
 fn test_parser() {
-    let input = "t(n(_, 0,block),t(n(_, 1,def),nil,t(n(_, 1,use),nil,nil)), nil).";
+    let input = "t(n(1, 0,block),t(n(2, 1,def),nil,t(n(3, 1,use),nil,nil)), nil).";
     let tree = utils::parser::get_tree(input);
     let s = SimpleAST::from_tree(tree);
     assert_eq!(s.len(), 1);
